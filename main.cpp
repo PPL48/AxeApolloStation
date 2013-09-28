c04c98d2937fde43c24ef52700750cfe201cc400
Satria Ady Pradana <master@xathrya.web.id>
2013-09-28 20:31:36 +0700

    Commit before merge

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include "viewfactory.h"
 #include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
     
+    MainWindow w;
+
+    ViewFactory::createInstance(&w);
+    ViewFactory::start();
+
     return a.exec();
 }
